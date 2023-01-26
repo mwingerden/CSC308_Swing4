b80c7c6d0516e230e49e23b89cd5715397eb05d7
Matthew Wingerden <matthew.wingerden@gmail.com>
2023-01-25 19:38:10 -0800

    Added Javadoc comments.

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,19 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Calculator class creates a basic looking calculator in Java Swing that can add, subtract, multiply, and divide
+ * only integer numbers.
+ *
+ * @author Matthew Wingerden
+ * @author Pablo Nguyen
+ * @author Juan Custodio
+ * @author Kevin Henderson
+ * @author Nathan Ho
+ * @version 1.0
+ * @since 2023-25-01
+ *
+ */
 public class Calculator extends JFrame implements ActionListener {
     private JTextField text;
     private JButton buttonAdd;
@@ -12,21 +25,32 @@ public class Calculator extends JFrame implements ActionListener {
     private String accumulator = "";
     private String operator = "";
 
+    /**
+     * The main method that creates an instance of the Calculator class and runs and displays GUI
+     *
+     **/
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         calc.createAndShowGUI();
     }
 
+    /**
+     * The method creates a GUI of a calculator. The method adds the text box and a grid with buttons
+     * to the window.
+     *
+     */
     public void createAndShowGUI() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel window = new JPanel(new GridLayout(2, 1));
+        JPanel window = new JPanel(new BorderLayout());
 
         text = new JTextField();
         text.setEditable(false);
-        window.add(text);
+        text.setFont(new Font("Helvetica", Font.PLAIN, 20));
+        text.setMinimumSize(new Dimension(100,500));
+        window.add(text, BorderLayout.NORTH);
 
-        JPanel calculator = new JPanel(new GridLayout(4,4));
+        JPanel calculator = new JPanel(new GridLayout(4,4, 10, 10));
 
         JButton button0 = new JButton("0");
         JButton button1 = new JButton("1");
@@ -79,105 +103,21 @@ public class Calculator extends JFrame implements ActionListener {
         calculator.add(buttonEquals);
         calculator.add(buttonDivide);
 
-        window.add(calculator);
+        window.add(calculator, BorderLayout.CENTER);
 
         add(window);
-//        JPanel jPanel = new JPanel(new GridBagLayout());
-//        GridBagConstraints c = new GridBagConstraints();
-//        add(jPanel);
-//        jTextField = new JTextField();
-//        jTextField.setEditable(false);
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//        c.weightx = 0;
-//        c.gridx = 0;
-//        c.gridy = 0;
-//        c.gridwidth = 4;
-//        jPanel.add(jTextField, c);
-//        add(jPanel);
-//
-//        JButton button0 = new JButton("0");JButton button1 = new JButton("1");
-//        JButton button2 = new JButton("2");JButton button3 = new JButton("3");
-//        JButton button4 = new JButton("4");JButton button5 = new JButton("5");
-//        JButton button6 = new JButton("6");JButton button7 = new JButton("7");
-//        JButton button8 = new JButton("8");JButton button9 = new JButton("9");
-//
-//        JButton buttonPlus = new JButton("+");JButton buttonMinus = new JButton("-");
-//        JButton buttonDivide = new JButton("/");JButton buttonMultiply = new JButton("x");
-//        JButton buttonClear = new JButton("C");
-//        JButton buttonEquals = new JButton("=");
-//
-//        c.weightx = .5;
-//        c.gridwidth = 1;
-//        c.gridx = 0;
-//        c.gridy = 1;
-//        button7.addActionListener(this);
-//        jPanel.add(button7, c);
-//        c.gridx = 1;
-//        c.gridy = 1;
-//        button8.addActionListener(this);
-//        jPanel.add(button8, c);
-//        c.gridx = 2;
-//        c.gridy = 1;
-//        button9.addActionListener(this);
-//        jPanel.add(button9, c);
-//        c.gridx = 3;
-//        c.gridy = 1;
-//        buttonPlus.addActionListener(this);
-//        jPanel.add(buttonPlus, c);
-//        c.gridx = 0;
-//        c.gridy = 2;
-//        button4.addActionListener(this);
-//        jPanel.add(button4, c);
-//        c.gridx = 1;
-//        c.gridy = 2;
-//        button5.addActionListener(this);
-//        jPanel.add(button5, c);
-//        c.gridx = 2;
-//        c.gridy = 2;
-//        button6.addActionListener(this);
-//        jPanel.add(button6, c);
-//        c.gridx = 3;
-//        c.gridy = 2;
-//        buttonMinus.addActionListener(this);
-//        jPanel.add(buttonMinus, c);
-//        c.gridx = 0;
-//        c.gridy = 3;
-//        button1.addActionListener(this);
-//        jPanel.add(button1, c);
-//        c.gridx = 1;
-//        c.gridy = 3;
-//        button2.addActionListener(this);
-//        jPanel.add(button2, c);
-//        c.gridx = 2;
-//        c.gridy = 3;
-//        button3.addActionListener(this);
-//        jPanel.add(button3, c);
-//        c.gridx = 3;
-//        c.gridy = 3;
-//        buttonMultiply.addActionListener(this);
-//        jPanel.add(buttonMultiply, c);
-//        c.gridx = 0;
-//        c.gridy = 4;
-//        buttonClear.addActionListener(this);
-//        jPanel.add(buttonClear, c);
-//        c.gridx = 1;
-//        c.gridy = 4;
-//        button0.addActionListener(this);
-//        jPanel.add(button0, c);
-//        c.gridx = 2;
-//        c.gridy = 4;
-//        buttonEquals.addActionListener(this);
-//        jPanel.add(buttonEquals, c);
-//        c.gridx = 3;
-//        c.gridy = 4;
-//        buttonDivide.addActionListener(this);
-//        jPanel.add(buttonDivide, c);
 
         setTitle("Calculator");
         setSize(450, 350);
         setVisible(true);
     }
 
+    /**
+     * This method contains implementation of a calculator that only does basic arithmetic on integers only.
+     * The method can only handle one operation at a time; you cannot add additional arithmetic after the
+     * conclusion of an operation.
+     *
+     **/
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
